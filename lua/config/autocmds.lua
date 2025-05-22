d9bdd06dcdb898f83c3d0381274093c042000a9a
Edwin Melara <edwin.melara.landaverde@gmail.com>
2025-05-22 00:08:27 -0600

    Configuration initial

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  command = "set nopaste",
+})
+
+-- vim.api.nvim_create_autocmd("FileTye", {
+--   pattern = { "json", "jsonc", "markdown" },
+--   callback = function()
+--     vim.opt.conceallevel = 0
+--   end,
+-- })
---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,14 @@
+return {
+  { import = "lazyvim.plugins.extras.linting.eslint" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+  { import = "lazyvim.plugins.extras.lang.ansible" },
+  { import = "lazyvim.plugins.extras.lang.terraform" },
+  { import = "lazyvim.plugins.extras.ai.copilot" },
+  { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+}
